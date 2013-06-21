cbed25b55a46f8f7bea8165a812bff7580ecf250
otiai10 <otiai10@gmail.com>
2013-06-21 21:30:20 +0900

    minimum

--- chat_app.py
+++ chat_app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import random
 from geventwebsocket.handler import WebSocketHandler
@@ -11,30 +12,26 @@ def chat_handle (environ, start_response):
   global cnt
   ws = environ['wsgi.websocket']
   ws_list.add(ws)
-  print '>>>>enter!', len(ws_list)
+  print '>>>>>>>>>>> ENTER', len(ws_list)
   while 1:
     # msg = ws.wait()
     # @see : http://www.gelens.org/code/gevent-websocket/
-    print 'Waiting......'
     msg = ws.receive()
-    print 'Received!!'
+    msg = msg.encode('utf-8')
     if msg is None:
       break
     remove = set()
     for s in ws_list:
       try:
-        msg = 'unko'
-        s.send(msg)
+        s.send("This is Server. I'v just got your message => " + msg)
       except Exception:
         remove.add(s)
     for s in remove:
       ws_list.remove(s)
-    break
-  print '<<<<exit!' , len(ws_list)
+  print '<<<<<<<<<< EXIT' , len(ws_list)
 
 def myapp (environ, start_response):
   path = environ["PATH_INFO"]
-  print path
   if path == '/':
     start_response('200 OK',[("Content-type","text/html")])
     return open('./src/view/index.html').read()
@@ -43,6 +40,9 @@ def myapp (environ, start_response):
   elif path == '/js/main.js':
     start_response('200 OK',[("Content-type","text/javascript")])
     return open('./src/js/main.js').read()
+  elif path == '/css/main.css':
+    start_response('200 OK',[("Content-type","text/css")])
+    return open('./src/css/main.css').read()
   elif path == '/favicon.ico':
     start_response('200 OK',[("Content-type","image/jpeg")])
     return open('./src/img/favicon.jpg').read()
@@ -51,5 +51,4 @@ def myapp (environ, start_response):
   #raise Exception('Not Found!')
 
 server = pywsgi.WSGIServer(('otiai10.com', 9090), myapp, handler_class = WebSocketHandler)
-
 server.serve_forever()
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,23 @@
 
 $(function(){
   socket = new WebSocket("ws://otiai10.com:9090/chat");
-  socket.onmessage = function(mess){
-    console.log(mess);
-  };
-  for (var i in socket) {
-    console.log(i, socket[i]);
+  socket.onmessage = function(ev){
+    //console.log('On Message Event =>', ev);
+    $('#stream').prepend('<li>' + ev.data + '</li>');
+  } 
+  socket.onopen = function(ev){
+    //console.log('On Open Event =>', ev);
+    socket.send('Hi, this is Client!!');
   }
+
+  // register click event
+  $("#submit").on('click', function(){
+      var mess = $("#message").val();
+      if (mess) {
+        socket.send(mess);
+        $("#message").val('');
+      } else {
+        console.log('突然のNULL String');
+      }
+  });
 });
