bdb6458cf77d68f95e48a28663fd86a7f0d8bf13
otiai10 <otiai10@gmail.com>
2013-06-23 13:30:16 +0900

    frontend

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,16 @@ $(function(){
   //socket = new WebSocket("ws://%{host}:9090/chat");
   socket = new WebSocket("ws://www15224uf.sakura.ne.jp:9090/chat");
   socket.onmessage = function(ev){
-    //console.log('On Message Event =>', ev);
     data = JSON.parse(ev.data);
-    $('#stream').prepend('<li>' + data.nickname + data.echo + '</li>');
+    console.log(data);
+    $('#stream').prepend('<li>' + data.nickname + ' (ﾟ⊿ﾟ) < ' +  data.echo + '</li>');
   } 
   socket.onopen = function(ev){
-    //console.log('On Open Event =>', ev);
     socket.send('Hi, this is Client!!');
   }
 
   // register click event
-  $("#submit").on('click', function(){
+  $(".submit").on('click', function(){
       var mess = $("#message").val();
       if (mess) {
         socket.send(mess);
