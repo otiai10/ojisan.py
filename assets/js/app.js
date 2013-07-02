dbc5f35f09de3beb34f1a381e93d2be23283a8cb
otiai10 <otiai10@gmail.com>
2013-07-02 16:53:32 +0900

    minimum client

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,14 @@ var __unread_cnt = 0;
 var __socket;
 
 $(function(){
-  $("#forms").hide();
+  alert('ready');
   __socket = new WebSocket("ws://oti10.com:9090/chat");
   __socket.onmessage = function(ev){
     console.log('message event => ', ev);
   } 
   __socket.onopen = function(ev){
     console.log('open event => ', ev);
-    mess = JSON.stringify({'request':'/me/id','params':null});
-    __socket.send(mess);
+    first_mess = JSON.stringify({'request':'/me/id','params':null});
+    __socket.send(first_mess);
   }
 });
