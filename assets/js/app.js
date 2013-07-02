b2524c618cdb19b37ed5be70a4928ae0aab69e56
otiai10 <otiai10@gmail.com>
2013-07-02 17:49:17 +0900

    fasten HTTP

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,34 @@ var __unread_cnt = 0;
 var __socket;
 
 (function(){
-  // 別にクライアントサイドで持つべきデータ無くね？
-  var Client = Backbone.Model.extend({
+  var Message = Backbone.Model.extend({
     defaults : {
-      name:'tmp'
+      sender  : 'otiai10',
+      content : 'おっぱい',
+      sent_at : '2013-07-02 17:26:56',
     }
   });
-  // Backbone.Viewは使えるかも
-  console.log(Client);
+  var Messages = Backbone.Collection.extend({model:Message});
+  var MessageView = Backbone.View.extend({
+    tagName  : 'li',
+    template : _.template($('#message').html()),
+    render   : function(){
+      var tpl = this.template(this.model.toJson());
+      this.$el.html(tpl);
+      return this;
+    },
+  });
+  var MessagesView = Backbone.View.extend({
+    tagName : 'ul',
+    render : function(){
+      this.collection.each(function(m){
+        var m = new MessageView({model:m});
+        this.$el.append(m.render().el);
+      },this);
+      return this;
+    },
+  });
+
   __socket = new WebSocket("ws://oti10.com:9090/chat");
   __socket.onmessage = function(ev){
     console.log('message event => ', ev);
