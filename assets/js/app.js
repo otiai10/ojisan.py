3a2534b11a13268dd19ceb345c5658ccec550ff7
otiai10 <otiai10@gmail.com>
2013-07-02 17:08:46 +0900

    for Backbone

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,15 @@ var __my_id = '';
 var __unread_cnt = 0;
 var __socket;
 
-$(function(){
-  alert('ready');
+(function(){
+  // 別にクライアントサイドで持つべきデータ無くね？
+  var Client = Backbone.Model.extend({
+    defaults : {
+      name:'tmp'
+    }
+  });
+  // Backbone.Viewは使えるかも
+  console.log(Client);
   __socket = new WebSocket("ws://oti10.com:9090/chat");
   __socket.onmessage = function(ev){
     console.log('message event => ', ev);
@@ -13,4 +20,4 @@ $(function(){
     first_mess = JSON.stringify({'request':'/me/id','params':null});
     __socket.send(first_mess);
   }
-});
+})();
